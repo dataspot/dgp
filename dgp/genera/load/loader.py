3c2d8000a72a5838a04acd772db2b59e3f224a84
Adam Kariv <adam.kariv@gmail.com>
2022-07-31 23:44:19 +0300

    v0.0.96 back to csv based caching

--- dgp/genera/load/loader.py
+++ dgp/genera/load/loader.py
@@ -3,7 +3,7 @@ import json
 import requests
 from hashlib import md5
 
-from dataflows import Flow, load, dump_to_path, checkpoint
+from dataflows import Flow, load, dump_to_path
 from dataflows.base.schema_validator import ignore
 
 from ...core import BaseDataGenusProcessor, Required, Validator, ConfigurableDGP
@@ -56,22 +56,17 @@ class LoaderDGP(BaseDataGenusProcessor):
                     loader,
                 )
             else:
+                if not os.path.exists(datapackage_path):
+                    logger.info('Caching source data into %s', cache_path)
+                    Flow(
+                        loader,
+                        dump_to_path(cache_path, validator_options=dict(on_error=ignore)),
+                        # printer(),
+                    ).process()
+                logger.info('Using cached source data from %s', cache_path)
                 return Flow(
-                    loader,
-                    checkpoint(ref_hash),
+                    load(datapackage_path, resources=RESOURCE_NAME),
                 )
-                # if not os.path.exists(cache_path):
-                #     logger.info('Caching source data into %s', cache_path)
-                #     Flow(
-                #         loader,
-                #         stream(cache_path),
-                #         # printer(),
-                #     ).process()
-                # logger.info('Using cached source data from %s', cache_path)
-                # return Flow(
-                #     unstream(cache_path),
-                #     # load(datapackage_path, resources=RESOURCE_NAME),
-                # )
 
 
 class PostLoaderDGP(ConfigurableDGP):
